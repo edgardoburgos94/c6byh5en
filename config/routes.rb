b5d0e678b979cc13c5e5b59f93678bfaedab05b7
Edgardo David <edgardo.burgos94@gmail.com>
2017-12-10 16:02:23 -0500

    change 'json' to ':json' in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :products
 
   Rails.application.routes.draw do
-    namespace :api, defaults: { format: "json" } do
+    namespace :api, defaults: { format: :json } do
       namespace :v1 do
         resources :products
       end
